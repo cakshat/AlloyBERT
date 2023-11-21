6ab596a591b3ec2e74793808929de2869e562811
CG_Lab <gchakradhar72@gmail.com>
2023-11-21 17:39:30 -0500

    subdirs

--- data/convertToPKL.py
+++ data/convertToPKL.py
@@ -5,23 +5,23 @@ from sklearn.model_selection import train_test_split
 dataset = 'MPEA_clean'
 
 # normalize YS, split into train and val sets
-data = pd.read_csv(f'./data/{dataset}.csv')
+data = pd.read_csv(f'./data/{dataset}/{dataset}.csv')
 
 if dataset == 'ys_clean':
     targetCol = 'YS'
-elif dataset == 'MPEA_clean':
+else:
     targetCol = 'PROPERTY: Calculated Young modulus (GPa)'
 
 data[targetCol] /= max(data[targetCol])
 tr, vl = train_test_split(data, test_size=0.15)
-tr.to_csv(f'./data/{dataset}_tr.csv')
-vl.to_csv(f'./data/{dataset}_vl.csv')
+tr.to_csv(f'./data/{dataset}/tr.csv')
+vl.to_csv(f'./data/{dataset}/vl.csv')
 
-# tr = pd.read_csv(f'./data/{dataset}_tr.csv')
+# tr = pd.read_csv(f'./data/{dataset}/tr.csv')
 ytr = tr[targetCol]
 Xtr = tr.drop(targetCol, axis=1)
 
-# vl = pd.read_csv(f'./data/{dataset}_vl.csv')
+# vl = pd.read_csv(f'./data/{dataset}/vl.csv')
 yvl = vl[targetCol]
 Xvl = vl.drop(targetCol, axis=1)
 
@@ -47,5 +47,5 @@ df_val = pd.DataFrame()
 df_val['text'] = textvl
 df_val['target'] = yvl.tolist()
 
-df_train.to_pickle(f'./data/{dataset}_tr.pkl')
-df_val.to_pickle(f'./data/{dataset}_vl.pkl')
+df_train.to_pickle(f'./data/{dataset}/tr.pkl')
+df_val.to_pickle(f'./data/{dataset}/vl.pkl')
