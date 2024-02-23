93c11ad14d4c44f030c18fde56e10e0cb504f29b
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-02-23 15:42:16 -0500

    Adds SVR and XGBoost

--- shallowML.py
+++ shallowML.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 
 
@@ -34,7 +35,7 @@ plt.savefig(f'./graphs/LR/{dataset}.png')
 
 
 # Random Forest
-rf = RandomForestRegressor(max_depth=2, random_state=0).fit(Xtr, ytr)
+rf = RandomForestRegressor(max_depth=10, random_state=0).fit(Xtr, ytr)
 yh2 = rf.predict(Xvl)
 RF_loss = np.mean((yh2 - yvl)**2)
 print('Random Forest Model:', RF_loss)
@@ -42,3 +43,25 @@ plt.clf()
 plt.plot(yh2)
 plt.plot(yvl.to_numpy())
 plt.savefig(f'./graphs/RF/{dataset}.png')
+
+
+# SVR
+svr = SVR(C=0.1).fit(Xtr, ytr)
+yh3 = svr.predict(Xvl)
+SVR_loss = np.mean((yh3 - yvl)**2)
+print('SVR Model:', SVR_loss)
+plt.clf()
+plt.plot(yh3)
+plt.plot(yvl.to_numpy())
+plt.savefig(f'./graphs/SVR/{dataset}.png')
+
+
+# Gradient Boosting
+gb = GradientBoostingRegressor(n_estimators=100, max_depth=20).fit(Xtr, ytr)
+yh4 = gb.predict(Xvl)
+GB_loss = np.mean((yh4 - yvl)**2)
+print('Gradient Boosting Model:', GB_loss)
+plt.clf()
+plt.plot(yh4)
+plt.plot(yvl.to_numpy())
+plt.savefig(f'./graphs/GB/{dataset}.png')
